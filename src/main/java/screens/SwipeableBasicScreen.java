cb8a2747399a83e5fa08387db1e4b59838a03631
TayaHatum <21taya21@gmail.com>
2022-05-24 18:29:13 +0300

    added swipe

--- src/main/java/screens/SwipeableBasicScreen.java
+++ src/main/java/screens/SwipeableBasicScreen.java
@@ -3,6 +3,10 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -16,14 +20,42 @@ public class SwipeableBasicScreen extends BaseScreen{
     List<MobileElement> list;
 
     public SwipeableBasicScreen swipeFromLeftToRight(int index){
+        Dimension size = driver.manage().window().getSize();
+        System.out.println(size.getHeight());
+        System.out.println(size.getWidth());
 
-        // home work
+        MobileElement element = list.get(index);
+        Rectangle rect = element.getRect();
+        int y = rect.getY()+ rect.getHeight()/2;
+        //int xFrom= rect.getX()+10;
+        int xFrom= rect.getX()+ rect.getWidth()/4;
+        //int xFrom= rect.getX()+ rect.getWidth()*10%;
+        int xTo= rect.getX() + (rect.getWidth()/4)*3;
+
+        TouchAction<?>touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(xFrom,y))
+                .moveTo(PointOption.point(xTo,y))
+                .release()
+                .perform();
+
         return this;
     }
 
     public SwipeableBasicScreen swipeFromRightToLeft(int index){
+        MobileElement element = list.get(index);
+        Rectangle rect = element.getRect();
+        int y = rect.getY()+ rect.getHeight()/2;
 
-        // home work
+        int xTo= rect.getX()+ rect.getWidth()/4;
+
+        int xFrom= rect.getX() + (rect.getWidth()/4)*3;
+
+        TouchAction<?>touchAction = new TouchAction<>(driver);
+        touchAction.longPress(PointOption.point(xFrom,y))
+                .moveTo(PointOption.point(xTo,y))
+                .release()
+                .perform();
+
         return this;
     }
 }
